f429bcc090a967b4a6d5a0be32c6d59dbffd7b34
Aymane Boujeddi <aymane655@gmail.com>
2025-11-24 12:18:01 +0100

    SMART-10 Créer l'entite User avec enum Role

---
+++ src/main/java/com/smartshop/entity/User.java
@@ -0,0 +1,39 @@
+package com.smartshop.entity;
+
+
+import com.smartshop.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password ;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+
+
+
+}
