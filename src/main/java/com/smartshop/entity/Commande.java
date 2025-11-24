bd1588e7a0f828d0362ec87de4bbe7b4b24f1e68
Aymane Boujeddi <aymane655@gmail.com>
2025-11-24 15:20:12 +0100

    fix(Commande) : updated Commande entity to include montantRestant and adjusted initialization logic

--- src/main/java/com/smartshop/entity/Commande.java
+++ src/main/java/com/smartshop/entity/Commande.java
@@ -2,6 +2,7 @@ package com.smartshop.entity;
 
 import com.smartshop.enums.StatutCommande;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,24 +23,34 @@ public class Commande {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    // le total de la commande avant les remises et tva
     @Column(nullable = false)
     private Double sousTotal;
 
+    // montant de reduction ex : 100 dh
     @Column(nullable = false)
     private Double montantRemise;
 
+    // taux de reduction ex : 5%
     @Column(nullable = false)
     private int remise;
 
+
+    // le taux tva : 20% updatable
     @Column(nullable = false)
     private int TVA;
 
+
+    @Pattern(regexp = "PROMO-[A-Z0-9]{4}")
+    private String codePromo;
+
+    // le total after tax calculated : totalPreTax + (totalPretax * TVA)
     @Column(nullable = false)
     private Double totalTTC;
 
+    // le total avant implementation du tax
     @Column(nullable = false)
-    private Double totalAPayer;
+    private Double totalPreTax;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -51,8 +62,14 @@ public class Commande {
     @Column(nullable = false)
     private LocalDateTime dateModification;
 
+
+    // pour le tracking de numero de paiment dans l'entite paiement
     @Column(nullable = false)
     private int numeroPaiement;
+
+    // montant restant a payer pour tracker le reste de paiment
+    @Column(nullable = false)
+    private Double montantRestant;
 
 
     @ManyToOne
@@ -72,6 +89,7 @@ public class Commande {
         this.dateCreation = LocalDateTime.now();
         this.dateModification = LocalDateTime.now();
         this.numeroPaiement = 0;
+        this.montantRestant = 0.0;
 
     }
 
