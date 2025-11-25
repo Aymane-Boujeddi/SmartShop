239089085b95e1a73bddb8e1230f94972183a055
Aymane Boujeddi <aymane655@gmail.com>
2025-11-25 09:51:23 +0100

    SMART-30 feat(dto): add AdminCreationDTO and UserResponseDTO for user management

---
+++ src/main/java/com/smartshop/dto/response/UserResponseDTO.java
@@ -0,0 +1,22 @@
+package com.smartshop.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+
+    private Long id;
+
+    private String username;
+
+    private String role;
+
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private ClientResponseDTO client;
+}
