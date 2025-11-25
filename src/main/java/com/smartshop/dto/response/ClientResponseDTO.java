e39fa1d7c413490c9822dd65b6f70cf9c6eaebfb
Aymane Boujeddi <aymane655@gmail.com>
2025-11-25 09:50:26 +0100

    SMART-20 feat(dto): add ClientCreationDTO and ClientResponseDTO for client management

---
+++ src/main/java/com/smartshop/dto/response/ClientResponseDTO.java
@@ -0,0 +1,27 @@
+package com.smartshop.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientResponseDTO {
+
+    private Long id;
+    private String nom;
+    private String email;
+    private String niveauFidelite;
+    private Double montantCumule;
+    private Integer totalCommandes;
+    private LocalDateTime dateCreation;
+    private LocalDateTime datePremiereCommande;
+    private LocalDateTime dateDerniereCommande;
+
+
+
+
+}
