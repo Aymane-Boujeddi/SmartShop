a4395e314b34703bdcace33bc1142c5a66ddac52
Aymane Boujeddi <aymane655@gmail.com>
2025-11-26 11:20:49 +0100

    SMART-42 feat(service): add ProduitService interface and its implementation

---
+++ src/main/java/com/smartshop/service/impl/ProduitServiceImpl.java
@@ -0,0 +1,8 @@
+package com.smartshop.service.impl;
+
+import com.smartshop.service.ProduitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProduitServiceImpl implements ProduitService {
+}
